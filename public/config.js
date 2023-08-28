607543dc959a26126d3aef15d60d57a87c1092b5
BillyBobJones678 <78831828+BillyBobJones678@users.noreply.github.com>
2023-08-27 21:00:39 -0400

    config.js

--- public/config.js
+++ public/config.js
@@ -1,5 +1,5 @@
 window.__CONFIG__ = {
   // url must NOT end with a slash
-  VITE_CORS_PROXY_URL: "",
-  VITE_TMDB_READ_API_KEY: ""
+  VITE_CORS_PROXY_URL: "https://hello-world-throbbing-rain-19d2.williamholmes678.workers.dev",
+  VITE_TMDB_READ_API_KEY: "4849b702431f172a94c8ed9a4a664d9a"
 };
